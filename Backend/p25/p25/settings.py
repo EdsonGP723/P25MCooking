56650d52c41aa132e3a779725b0640fdfa558804
Edson Gutirrez <egutierrez@ethicsglobal.com>
2026-08-24 21:09:54 -0600

    Finalizando proyecto

--- Backend/p25/p25/settings.py
+++ Backend/p25/p25/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     # Habilita peticiones desde el Frontend
     'corsheaders.middleware.CorsMiddleware',
